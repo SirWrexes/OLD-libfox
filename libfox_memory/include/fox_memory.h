33c418796bb1f39514daaeaf8fb770e8ec71cd56
Renard <ludovic.fernandez@epitech.eu>
2019-02-18 13:50:57 +0100

    Added fox_allocsize   - fox_allocsize : get the number of allocated bytes for a pointer   --- This is mainly a collection of macros extracted from libc malloc.c
    Updated fox_calloc and fox_memset

--- libfox_memory/include/fox_memory.h
+++ libfox_memory/include/fox_memory.h
@@ -25,10 +25,23 @@ void fox_autoclose_fd(int *fd)  __attribute__ ((nonnull));
 void fox_autoclose_fp(FILE **fp);
 #endif
 
+// Get the number of allocated bytes for a pointer
+__attribute__ ((pure))
+size_t fox_allocbytes(void *ptr);
+
 // Set pointed memory to given data
 __attribute__ ((nonnull, returns_nonnull))
 void *fox_memset(void *ptr, size_t n, char data);
 
+// Allocate a pointer to n memebers of size and memset it to 0
+__attribute__ ((malloc, alloc_size(1, 2)))
+void *fox_calloc(size_t n, size_t size);
 
+// Change size of allocated memory
+//  -- Reallocating to 0 is like calling free.
+//  -- Reallocating a null pointer is like calling malloc
+// This function may cause conditional jump errors
+__attribute__ ((alloc_size(2)))
+void *fox_realloc(void *ptr, size_t newsize);
 
 #endif //LIBFOX_FOX_MEMORY_H
---
+++ libfox_memory/src/fox_realloc.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2018
+** libfox
+** File description:
+** No description
+*/
+
+#include "private/__foxmemory.h"
+#include "fox_memory.h"
+
+__attribute__ ((alloc_size(2)))
+void *fox_realloc(void *ptr, size_t newsize)
+{
+    void *newptr = NULL;
+    char *tmpdst = NULL;
+    char *tmpsrc = (char *) ptr;
+
+    if (ptr == NULL)
+        return malloc(newsize);
+    else if (newsize == 0) {
+        free(ptr);
+        return NULL;
+    } else {
+        newptr = malloc(newptr);
+
+    }
+}

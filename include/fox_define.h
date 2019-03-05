985e78ff0cb03fbd2c5efe9eb659c0cd592f7b53
Renard <ludovic.fernandez@epitech.eu>
2019-03-05 18:33:22 +0100

    Added __transparent (transp. union type attribute)

--- include/fox_define.h
+++ include/fox_define.h
@@ -13,15 +13,16 @@
 ** unless they depend on another macro's previous
 ** definition and follow this template  :
 **
-**     //What purpose the macro serves
-**     # define NAME (instruction)
+**     // What purpose the macro serves
+**     #define NAME (instruction)
 */
 
 // First we need to make sure their
 // name isn't already defined elsewhere
 #ifndef LIBFOX_MACRO_UNDEF
     #define LIBFOX_MACRO_UNDEF
-    #undef __A
+    #undef __a
+    #undef __transparent
     #undef CTOI
     #undef ITOC
     #undef CHAR_TOUPPER
@@ -37,47 +38,51 @@
     #undef IS_OP
     #undef ABS
     #undef MAX
+    #undef RET_NULL
 #endif // LIBFOX_MACRO_UNDEF
 
-//Then we can actually start to define them.
+// Then we can actually start to define them.
 #ifndef LIBFOX_MACRO_DEF
     #define LIBFOX_MACRO_DEF
 
-    //Shorter way of writing __attribute__
-    #define __A(attributes) __attribute__((attributes))
+//  Shorter way of writing __attribute__
+    #define __a(attributes) __attribute__(attributes)
 
-    //Digit ascii value to numeric value
+//  See https://gcc.gnu.org/onlinedocs/gcc-3.3/gcc/Type-Attributes.html
+    #define __trasnparent __a((__transparent_union__))
+
+//  Digit ascii value to numeric value
     #define CTOI(c) ((int) ((c) - '0'))
 
-    //Numeric value to ascii char value
+//  Numeric value to ascii char value
     #define ITOC(i) ((char) ((i) + '0'))
 
-    //If an alphabetical character is lowercase, make it uppercase
+//  If an alphabetical character is lowercase, make it uppercase
     #define CHAR_TOUPPER(c)  ((c) >= 'a' && (c) <= 'z' ? (c) - 32 : (c))
 
-    //If an alphabetical character is uppercase, make it lowercase
+//  If an alphabetical character is uppercase, make it lowercase
     #define CHAR_TOLOWER(c)  ((c) >= 'A' && (c) <= 'Z' ? (c) - 32 : (c))
 
-    //Check if a char is an end of line (linebreak OR null char)
+//  Check if a char is an end of line (linebreak OR null char)
     #define IS_EOL(c) ((c) == '\n' || (c) == '\0')
 
-    //Check if a char is strictly lowercase alphabetical
+//  Check if a char is strictly lowercase alphabetical
     #define IS_ALPHALO(c) ((c) >= 'a' && (c) <= 'z')
 
-    //Check if a char is strictly uppercase alphabetical
+//  Check if a char is strictly uppercase alphabetical
     #define IS_ALPHAUP(c) ((c) >= 'A' && (c) <= 'Z')
 
-    //Check if a char is strictly alphabetical
+//  Check if a char is strictly alphabetical
     #define IS_ALPHA(c) (IS_ALPHALO(c) || IS_ALPHAUP(c))
 
-    //Check if a char is strictly numeric
+//  Check if a char is strictly numeric
     #define IS_NUM(c) ((c) >= '0' && (c) <= '9')
 
-    //Check if a char is strictly alphanumeric
+//  Check if a char is strictly alphanumeric
     #define IS_ALPHANUM(c) (IS_ALPHA(c) || IS_NUM(c))
 
-    //Check if a char is punctuation
-    #define IS_PUNCT(c)  \
+//  Check if a char is punctuation
+    #define IS_PUNCT(c)     \
     (                       \
         ((c) == '\'')       \
         || ((c) == '\"')    \
@@ -93,14 +98,18 @@
         || ((c) == '/')     \
     )                       \
 
-    //Check if a char is a sign (+ OR -)
+//  Check if a char is a sign (+ OR -)
     #define IS_SIGN(c) ((c) == '+' || (c) == '-')
 
-    //Get the absolute value of a number
+//  Get the absolute value of a number
     #define ABS(x) ((x) >= 0 ? (x) : -(x))
 
-    //Get the biggest of two numbers
+//  Get the biggest of two numbers
     #define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+//  Execute a non-void function then return null no matter what
+//  -- Useful when you want to log an error and return null in 1 line
+    #define RET_NULL(func) ((func) ? (void *)0 : (void*)0)
 #endif // LIBFOX_MACRO_DEF
 
 #ifndef CCSTR_TYPE
@@ -109,6 +118,6 @@
     typedef char const * const          str3c_t;
     typedef char const *                str2c_t;
     typedef char *                      str_t;
-#endif //CCSTR_TYPE
+#endif // CCSTR_TYPE
 
-#endif //FOX_MACRO
+#endif // FOX_MACRO

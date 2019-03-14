12cd423a0349e504936054332d283fc2dd6c1911
Renard <ludovic.fernandez@epitech.eu>
2019-03-14 18:01:48 +0100

    Removed redundant macro definitions

--- source/libfox_string/include/private/p_foxstring.h
+++ source/libfox_string/include/private/p_foxstring.h
@@ -12,33 +12,10 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <malloc.h>
+#include "fox_define.h"
 
-#if !(defined FOX_MACRO) && !(defined FOX_STRING_MACRO)
-    #define FOX_STRING_MACRO
-    #ifndef __A
-        # define __A(x) __attribute__(x)
-    #endif
-    #define IS_ALPHALO(c) ((c) >= 'a' && (c) <= 'z')
-    #define IS_ALPHAUP(c) ((c) >= 'A' && (c) <= 'Z')
-    #define IS_ALPHA(c) (IS_ALPHALO(c) || IS_ALPHAUP(c))
-    #define IS_NUM(c) ((c) >= '0' && (c) <= '9')
-    #define IS_ALPHANUM(c) (IS_ALPHA(c) || IS_NUM(c))
-    #define IS_EOL(c) ((c) == '\n' || (c) == '\0')
-    #define IS_SIGN(c) ((c) == '+' || (c) == '-')
-    #define CHAR_TOUPPER(c)  ((c) >= 'a' && (c) <= 'z' ? (c) - 32 : (c))
-    #define CTOI(c) (int) ((c) - '0')
-#endif
-
-#ifndef CCSTR_TYPE
-    #define CCSTR_TYPE
-    typedef char const * const * const  str4c_t;
-    typedef char const * const          str3c_t;
-    typedef char const *                str2c_t;
-    typedef char *                      str_t;
-#endif //CCSTR_TYPE
-
-size_t fox_strlen(str3c_t str)      __A((pure));
-str_t fox_revstr(str_t str)         __A((nonnull));
-long fox_pow(long nb, size_t power) __A((const));
+size_t fox_strlen(str3c_t str)      __a((pure));
+str_t fox_revstr(str_t str)         __a((nonnull));
+long fox_pow(long nb, size_t power) __a((const));
 
 #endif //LIBFOX_FOXSTRING_H

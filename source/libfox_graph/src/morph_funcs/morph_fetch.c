266ff3930b1d9bccb9be7f1b1c14f5b0da7f2d89
Renard <ludovic.fernandez@epitech.eu>
2019-03-21 17:13:04 +0100

    <feat>(graph): Addd polymorphic graphfetch function
    void *fox_graphfetch(pmorph_t thing, void *in);
    Fetches 'thing' in 'in'.
    'in' can be an alist_t or graph_t
    'thing' can be anything vt->fetch can handle

---
+++ source/libfox_graph/src/morph_funcs/morph_fetch.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** libfox
+** File description:
+** Polymorphic graph fetch
+*/
+
+#include "private/p_foxgraph.h"
+
+extern void *fox_graphfetch(pmorph_t thing, void *in)
+{
+    switch (((graph_t) in)->type) {
+        default :
+            fox_eputs("graphfetch(): invalid container type");
+            return NULL;
+        case MT_GRAPH : return ((graph_t) in)->vt->fetch(in, thing);
+        case MT_LIST : return ((alist_t) in)->vt->fetch(in, thing);
+    }
+}

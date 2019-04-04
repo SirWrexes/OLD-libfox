d7d27ba808edac173ef31f4e59e588a77c926b58
Renard <ludovic.fernandez@epitech.eu>
2019-04-04 13:52:42 +0200

    <feat/refactor>(String) Enhance fox_strtok
    <feat>
      Added the option to simply change stock by sending a pointer in str
      and NULL in delims. Removed nonnull attribute.
    
    <refactor>
      Made lines 26-29 condition into a simpler one-liner.

--- source/string/src/fox_strtok.c
+++ source/string/src/fox_strtok.c
@@ -16,26 +16,25 @@ str_t fox_strtok_r(str_t str, str3c_t delims, str_t *stock)
         return NULL;
     if (str != NULL && str != *stock)
         *stock = str;
-    str = str ? : *stock;
+    str = (str != NULL) ? str : *stock;
     str += fox_strspn(str, delims);
     if (*str == '\0') {
         *stock = str;
         return NULL;
     }
     end = str + fox_strcspn(str, delims);
-    if (*end == '\0') {
-        *stock = end;
-        return str;
-    }
+    *stock = end + (*end == '\0');
     *end = '\0';
-    *stock = end + 1;
     return str;
 }
 
-__a((nonnull(2)))
 str_t fox_strtok(str_t str, str3c_t delims)
 {
     static str_t stock = NULL;
 
+    if (str != NULL && delims == NULL) {
+        stock = str;
+        return stock;
+    }
     return fox_strtok_r(str, delims, &stock);
 }
